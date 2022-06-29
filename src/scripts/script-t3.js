0593221a857dad47b7ba3c5e9b0476af604d34d7
Daniel <danielpqb@gmail.com>
2022-06-29 20:36:43 -0300

    Ajustes layout mobile T3

--- src/scripts/script-t3.js
+++ src/scripts/script-t3.js
@@ -10,134 +10,51 @@ let T3_1 = `
 
 let T3_2 = `
 <div class="T3-header">Crie suas perguntas</div>
-        <div class="T3-box">
-            <div class="T3-b1">
-                <div class="T3-h">
-                    <div>
-                        <h1>Pergunta 1</h1>
-                        <img class="T3-icon" onclick="toggleExpandQuestion(this)" src="img/edit.svg">
-                    </div>
-                    <input type="text" placeholder="Texto da pergunta">
-                    <input type="text" placeholder="Cor de fundo da pergunta">
-                </div>
-                <div class="T3-correctAnswer">
-                    <h1>Resposta correta</h1>
-                    <input type="text" placeholder="Resposta correta">
-                    <input type="text" placeholder="URL da imagem">
-                </div>
-                <div class="T3-incorrectAnswer1">
-                    <h1>Respostas incorretas</h1>
-                    <input type="text" placeholder="Resposta incorreta 1">
-                    <input type="text" placeholder="URL da imagem 1">
-                </div>
-                <div class="T3-incorrectAnswer2">
-                    <input type="text" placeholder="Resposta incorreta 2">
-                    <input type="text" placeholder="URL da imagem 2">
-                </div>
-                <div class="T3-incorrectAnswer3">
-                    <input type="text" placeholder="Resposta incorreta 3">
-                    <input type="text" placeholder="URL da imagem 3">
-                </div>
+<div class="T3-box">
+    <div class="T3-b">
+        <div class="T3-h">
+            <div>
+                <h1>Pergunta 1</h1>
+                <img class="T3-icon" onclick="toggleExpandQuestion(this)" src="img/edit.svg">
             </div>
-
-            <div class="T3-b2">
-                <div class="T3-h">
-                    <div>
-                        <h1>Pergunta 2</h1>
-                        <img class="T3-icon" onclick="toggleExpandQuestion(this)" src="img/edit.svg">
-                    </div>
-                    <input type="text" placeholder="Texto da pergunta">
-                    <input type="text" placeholder="Cor de fundo da pergunta">
-                </div>
-                <div class="T3-correctAnswer">
-                    <h1>Resposta correta</h1>
-                    <input type="text" placeholder="Resposta correta">
-                    <input type="text" placeholder="URL da imagem">
-                </div>
-                <div class="T3-incorrectAnswer1">
-                    <h1>Respostas incorretas</h1>
-                    <input type="text" placeholder="Resposta incorreta 1">
-                    <input type="text" placeholder="URL da imagem 1">
-                </div>
-                <div class="T3-incorrectAnswer2">
-                    <input type="text" placeholder="Resposta incorreta 2">
-                    <input type="text" placeholder="URL da imagem 2">
-                </div>
-                <div class="T3-incorrectAnswer3">
-                    <input type="text" placeholder="Resposta incorreta 3">
-                    <input type="text" placeholder="URL da imagem 3">
-                </div>
-            </div>
-
-            <div class="T3-b3">
-                <div class="T3-h">
-                    <div>
-                        <h1>Pergunta 3</h1>
-                        <img class="T3-icon" onclick="toggleExpandQuestion(this)" src="img/edit.svg">
-                    </div>
-                    <input type="text" placeholder="Texto da pergunta">
-                    <input type="text" placeholder="Cor de fundo da pergunta">
-                </div>
-                <div class="T3-correctAnswer">
-                    <h1>Resposta correta</h1>
-                    <input type="text" placeholder="Resposta correta">
-                    <input type="text" placeholder="URL da imagem">
-                </div>
-                <div class="T3-incorrectAnswer1">
-                    <h1>Respostas incorretas</h1>
-                    <input type="text" placeholder="Resposta incorreta 1">
-                    <input type="text" placeholder="URL da imagem 1">
-                </div>
-                <div class="T3-incorrectAnswer2">
-                    <input type="text" placeholder="Resposta incorreta 2">
-                    <input type="text" placeholder="URL da imagem 2">
-                </div>
-                <div class="T3-incorrectAnswer3">
-                    <input type="text" placeholder="Resposta incorreta 3">
-                    <input type="text" placeholder="URL da imagem 3">
-                </div>
-            </div>
-
         </div>
-        <div class="T3-btn" onclick="btnCreateQuizz(this)">Prosseguir pra criar níveis</div>`
+        <div class="T3-m">
+            <input type="text" placeholder="Texto da pergunta">
+            <input type="text" placeholder="Cor de fundo da pergunta">
+            <div class="T3-correctAnswer">
+                <h1>Resposta correta</h1>
+                <input type="text" placeholder="Resposta correta">
+                <input type="text" placeholder="URL da imagem">
+            </div>
+            <div class="T3-incorrectAnswer1">
+                <h1>Respostas incorretas</h1>
+                <input type="text" placeholder="Resposta incorreta 1">
+                <input type="text" placeholder="URL da imagem 1">
+            </div>
+            <div class="T3-incorrectAnswer2">
+                <input type="text" placeholder="Resposta incorreta 2">
+                <input type="text" placeholder="URL da imagem 2">
+            </div>
+            <div class="T3-incorrectAnswer3">
+                <input type="text" placeholder="Resposta incorreta 3">
+                <input type="text" placeholder="URL da imagem 3">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="T3-btn" onclick="btnCreateQuizz(this)">Prosseguir pra criar níveis</div>`
 
 let T3_3 = `
 <div class="T3-header">Agora, decida os níveis!</div>
 <div class="T3-box">
-    <div class="T3-b1">
+    <div class="T3-b">
         <div class="T3-h">
             <div>
                 <h1>Nível 1</h1>
                 <img class="T3-icon" onclick="toggleExpandLevel(this)" src="img/edit.svg">
             </div>
-            <input type="text" placeholder="Título do nível">
-            <input type="text" placeholder="% de acerto mínima">
-            <input type="text" placeholder="URL da imagem do nível">
-            <textarea type="text" placeholder="Descrição do nível" rows="5"></textarea>
         </div>
-    </div>
-</div>
-<div class="T3-box">
-    <div class="T3-b2">
-        <div class="T3-h">
-            <div>
-                <h1>Nível 2</h1>
-                <img class="T3-icon" onclick="toggleExpandLevel(this)" src="img/edit.svg">
-            </div>
-            <input type="text" placeholder="Título do nível">
-            <input type="text" placeholder="% de acerto mínima">
-            <input type="text" placeholder="URL da imagem do nível">
-            <textarea type="text" placeholder="Descrição do nível" rows="5"></textarea>
-        </div>
-    </div>
-</div>
-<div class="T3-box">
-    <div class="T3-b3">
-        <div class="T3-h">
-            <div>
-                <h1>Nível 3</h1>
-                <img class="T3-icon" onclick="toggleExpandLevel(this)" src="img/edit.svg">
-            </div>
+        <div class="T3-m">
             <input type="text" placeholder="Título do nível">
             <input type="text" placeholder="% de acerto mínima">
             <input type="text" placeholder="URL da imagem do nível">
